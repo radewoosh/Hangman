f1451a5274364b745a0a02dbe5468773476ebffa
vikas yadav <yadavvikas507@gmail.com>
2018-02-28 12:15:48 +0530

    Add files via upload

--- hangman/lib/run_game.ex
+++ hangman/lib/run_game.ex
@@ -9,25 +9,37 @@ defmodule Hangman.Lib.Rungame do
 	position = 0 
 	IO.puts "string is of length #{len}"
 	string = String.graphemes( string )
-	IO.inspect string
 	input_elem = []
 	make_guess( string , chances , input_elem , position )      
 
     end
 
     def make_guess( string , chances , input_elem , position ) when chances > 1 do
-    	input_char = IO.gets(" make your guess ")
+    	
+		
+		IO.puts "entered words till now"
+		IO.inspect input_elem
+
+		input_char = IO.gets(" make your guess ")
 		             |> String.trim("\n")
 
-		input_elem = input_elem ++ [input_char] 
+
+		input_elem = input_elem ++ [input_char]
+		             |> Enum.uniq
+
+		
 		print_curr_result( string , input_char , input_elem) 
 
 		if Enum.count( string , fn x -> x == input_char end) == 0 do
 			chances = chances - 1
 			position = position + 1
-		end
 
-		#IO.inspect input_elem
+			IO.puts "wrong guess !!!"
+			IO.puts Hangman.Lib.Print.print(  position ) 
+			make_guess( string , chances , input_elem , position)
+
+		else 
+
 		IO.puts Hangman.Lib.Print.print(  position )
 
 		if Enum.all?( string , fn x -> Enum.member?( input_elem , x ) == true end) do
@@ -35,21 +47,34 @@ defmodule Hangman.Lib.Rungame do
 		else
 		make_guess( string , chances , input_elem , position)
 	    end
-
+	end
     end
 
     def make_guess( string , chances , input_elem , position ) when chances == 1 do
-    	input_char = IO.gets("  make your guess ")
+    	
+		IO.puts "entered words till now"
+		IO.inspect input_elem
+
+        input_char = IO.gets("  make your guess ")
 		             |> String.trim("\n")
 
+
 		input_elem = input_elem ++ [input_char]
+		             |> Enum.uniq
+
 		if Enum.count( string , fn x -> x == input_char end) == 0 do
 			IO.puts " game over .. you lose the game "
+			IO.puts "answer is "
+			IO.puts List.to_string( string )
 			position = position + 1
+			IO.puts Hangman.Lib.Print.print( position )
+			Hangman.Lib.Rungame.start
 		else
 
 		 if Enum.all?( string , fn x -> Enum.member?( input_elem , x ) == true end) do
 			IO.puts " you won the game "
+			IO.puts Hangman.Lib.Print.print( position )
+			Hangman.Lib.Rungame.start
 		 else
 
 		 print_curr_result( string , chances , input_elem )
@@ -58,7 +83,7 @@ defmodule Hangman.Lib.Rungame do
 		end
 	  end
 
-	  IO.puts Hangman.Lib.Print.print( position )
+	  
 
     end
 
